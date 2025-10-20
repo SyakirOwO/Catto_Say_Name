cfdd08eebc96533e5bd9933599143f7442e7a66e
Syakirah <90323672+SyakirOwO@users.noreply.github.com>
2025-10-20 23:06:52 +0800

    Add files via upload
    This is the code from vstudio

---
+++ Textbox_Control/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.picGrass = New System.Windows.Forms.PictureBox()
+        Me.picDownCloud = New System.Windows.Forms.PictureBox()
+        Me.picDownMiniCloud = New System.Windows.Forms.PictureBox()
+        Me.picUpMiniCloud = New System.Windows.Forms.PictureBox()
+        Me.picUpCloud = New System.Windows.Forms.PictureBox()
+        Me.picSun = New System.Windows.Forms.PictureBox()
+        Me.picCatto = New System.Windows.Forms.PictureBox()
+        Me.picBubble = New System.Windows.Forms.PictureBox()
+        Me.picSmolBubble = New System.Windows.Forms.PictureBox()
+        Me.picTiangR = New System.Windows.Forms.PictureBox()
+        Me.picTiangL = New System.Windows.Forms.PictureBox()
+        Me.picBoard = New System.Windows.Forms.PictureBox()
+        Me.lblTask = New System.Windows.Forms.Label()
+        Me.btnGreet = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblGreetings = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        CType(Me.picGrass, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDownCloud, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDownMiniCloud, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picUpMiniCloud, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picUpCloud, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSun, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picCatto, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBubble, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSmolBubble, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTiangR, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTiangL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoard, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picGrass
+        '
+        Me.picGrass.BackColor = System.Drawing.Color.Green
+        Me.picGrass.Location = New System.Drawing.Point(-2, 293)
+        Me.picGrass.Name = "picGrass"
+        Me.picGrass.Size = New System.Drawing.Size(804, 86)
+        Me.picGrass.TabIndex = 0
+        Me.picGrass.TabStop = False
+        '
+        'picDownCloud
+        '
+        Me.picDownCloud.BackColor = System.Drawing.Color.White
+        Me.picDownCloud.Location = New System.Drawing.Point(499, 87)
+        Me.picDownCloud.Name = "picDownCloud"
+        Me.picDownCloud.Size = New System.Drawing.Size(229, 34)
+        Me.picDownCloud.TabIndex = 1
+        Me.picDownCloud.TabStop = False
+        '
+        'picDownMiniCloud
+        '
+        Me.picDownMiniCloud.BackColor = System.Drawing.Color.White
+        Me.picDownMiniCloud.Location = New System.Drawing.Point(60, 68)
+        Me.picDownMiniCloud.Name = "picDownMiniCloud"
+        Me.picDownMiniCloud.Size = New System.Drawing.Size(204, 29)
+        Me.picDownMiniCloud.TabIndex = 2
+        Me.picDownMiniCloud.TabStop = False
+        '
+        'picUpMiniCloud
+        '
+        Me.picUpMiniCloud.BackColor = System.Drawing.Color.White
+        Me.picUpMiniCloud.Location = New System.Drawing.Point(82, 34)
+        Me.picUpMiniCloud.Name = "picUpMiniCloud"
+        Me.picUpMiniCloud.Size = New System.Drawing.Size(143, 37)
+        Me.picUpMiniCloud.TabIndex = 3
+        Me.picUpMiniCloud.TabStop = False
+        '
+        'picUpCloud
+        '
+        Me.picUpCloud.BackColor = System.Drawing.Color.White
+        Me.picUpCloud.Location = New System.Drawing.Point(553, 34)
+        Me.picUpCloud.Name = "picUpCloud"
+        Me.picUpCloud.Size = New System.Drawing.Size(154, 53)
+        Me.picUpCloud.TabIndex = 4
+        Me.picUpCloud.TabStop = False
+        '
+        'picSun
+        '
+        Me.picSun.BackColor = System.Drawing.Color.Yellow
+        Me.picSun.Location = New System.Drawing.Point(408, 26)
+        Me.picSun.Name = "picSun"
+        Me.picSun.Size = New System.Drawing.Size(61, 61)
+        Me.picSun.TabIndex = 6
+        Me.picSun.TabStop = False
+        '
+        'picCatto
+        '
+        Me.picCatto.BackColor = System.Drawing.Color.Transparent
+        Me.picCatto.Image = CType(resources.GetObject("picCatto.Image"), System.Drawing.Image)
+        Me.picCatto.Location = New System.Drawing.Point(631, 143)
+        Me.picCatto.Name = "picCatto"
+        Me.picCatto.Size = New System.Drawing.Size(159, 144)
+        Me.picCatto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picCatto.TabIndex = 7
+        Me.picCatto.TabStop = False
+        '
+        'picBubble
+        '
+        Me.picBubble.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.picBubble.Location = New System.Drawing.Point(431, 143)
+        Me.picBubble.Name = "picBubble"
+        Me.picBubble.Size = New System.Drawing.Size(169, 70)
+        Me.picBubble.TabIndex = 8
+        Me.picBubble.TabStop = False
+        '
+        'picSmolBubble
+        '
+        Me.picSmolBubble.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.picSmolBubble.Location = New System.Drawing.Point(596, 202)
+        Me.picSmolBubble.Name = "picSmolBubble"
+        Me.picSmolBubble.Size = New System.Drawing.Size(29, 11)
+        Me.picSmolBubble.TabIndex = 9
+        Me.picSmolBubble.TabStop = False
+        '
+        'picTiangR
+        '
+        Me.picTiangR.BackColor = System.Drawing.Color.Black
+        Me.picTiangR.Location = New System.Drawing.Point(323, 87)
+        Me.picTiangR.Name = "picTiangR"
+        Me.picTiangR.Size = New System.Drawing.Size(17, 258)
+        Me.picTiangR.TabIndex = 10
+        Me.picTiangR.TabStop = False
+        '
+        'picTiangL
+        '
+        Me.picTiangL.BackColor = System.Drawing.Color.Black
+        Me.picTiangL.Location = New System.Drawing.Point(164, 87)
+        Me.picTiangL.Name = "picTiangL"
+        Me.picTiangL.Size = New System.Drawing.Size(17, 258)
+        Me.picTiangL.TabIndex = 11
+        Me.picTiangL.TabStop = False
+        '
+        'picBoard
+        '
+        Me.picBoard.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.picBoard.Location = New System.Drawing.Point(129, 103)
+        Me.picBoard.Name = "picBoard"
+        Me.picBoard.Size = New System.Drawing.Size(258, 164)
+        Me.picBoard.TabIndex = 12
+        Me.picBoard.TabStop = False
+        '
+        'lblTask
+        '
+        Me.lblTask.AutoSize = True
+        Me.lblTask.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblTask.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTask.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblTask.Location = New System.Drawing.Point(187, 133)
+        Me.lblTask.Name = "lblTask"
+        Me.lblTask.Size = New System.Drawing.Size(153, 20)
+        Me.lblTask.TabIndex = 13
+        Me.lblTask.Text = "Enter your Name :"
+        '
+        'btnGreet
+        '
+        Me.btnGreet.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnGreet.Location = New System.Drawing.Point(173, 202)
+        Me.btnGreet.Name = "btnGreet"
+        Me.btnGreet.Size = New System.Drawing.Size(72, 34)
+        Me.btnGreet.TabIndex = 15
+        Me.btnGreet.Text = "Show Greeting"
+        Me.btnGreet.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnExit.Location = New System.Drawing.Point(274, 201)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(66, 35)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lblGreetings
+        '
+        Me.lblGreetings.AutoSize = True
+        Me.lblGreetings.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblGreetings.Location = New System.Drawing.Point(445, 158)
+        Me.lblGreetings.Name = "lblGreetings"
+        Me.lblGreetings.Size = New System.Drawing.Size(13, 13)
+        Me.lblGreetings.TabIndex = 17
+        Me.lblGreetings.Text = "  "
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(155, 167)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(199, 20)
+        Me.txtName.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 379)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblGreetings)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGreet)
+        Me.Controls.Add(Me.lblTask)
+        Me.Controls.Add(Me.picBoard)
+        Me.Controls.Add(Me.picTiangL)
+        Me.Controls.Add(Me.picTiangR)
+        Me.Controls.Add(Me.picSmolBubble)
+        Me.Controls.Add(Me.picBubble)
+        Me.Controls.Add(Me.picCatto)
+        Me.Controls.Add(Me.picSun)
+        Me.Controls.Add(Me.picUpCloud)
+        Me.Controls.Add(Me.picUpMiniCloud)
+        Me.Controls.Add(Me.picDownMiniCloud)
+        Me.Controls.Add(Me.picDownCloud)
+        Me.Controls.Add(Me.picGrass)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picGrass, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDownCloud, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDownMiniCloud, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picUpMiniCloud, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picUpCloud, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSun, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picCatto, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBubble, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSmolBubble, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTiangR, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTiangL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoard, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picGrass As PictureBox
+    Friend WithEvents picDownCloud As PictureBox
+    Friend WithEvents picDownMiniCloud As PictureBox
+    Friend WithEvents picUpMiniCloud As PictureBox
+    Friend WithEvents picUpCloud As PictureBox
+    Friend WithEvents picSun As PictureBox
+    Friend WithEvents picCatto As PictureBox
+    Friend WithEvents picBubble As PictureBox
+    Friend WithEvents picSmolBubble As PictureBox
+    Friend WithEvents picTiangR As PictureBox
+    Friend WithEvents picTiangL As PictureBox
+    Friend WithEvents picBoard As PictureBox
+    Friend WithEvents lblTask As Label
+    Friend WithEvents btnGreet As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblGreetings As Label
+    Friend WithEvents txtName As TextBox
+End Class
---
+++ Textbox_Control/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnGreet_Click(sender As Object, e As EventArgs) Handles btnGreet.Click
+        Dim userInput As String
+        userInput = txtName.Text
+        lblGreetings.Text = "Hello " & userInput & "."
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub picCatto_Click(sender As Object, e As EventArgs) Handles picCatto.Click
+        lblGreetings.Text = "Meow ! !"
+    End Sub
+
+    Private Sub picSun_Click(sender As Object, e As EventArgs) Handles picSun.Click
+        lblGreetings.Text = "Panasnya harini..."
+    End Sub
+
+
+End Class
